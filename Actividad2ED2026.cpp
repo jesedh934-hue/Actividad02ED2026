0fb13959c08ca9a0dac874f8a7a419413f106164
jesedh934-hue <jesedh934@gmail.com>
2026-01-20 22:03:42 -0600

    Create Actividad2ED2026.cpp

---
+++ Actividad2ED2026.cpp
@@ -0,0 +1,10 @@
+// Actividad02ED2026.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello Wordl!, LIDTS 2-O Estructura de datos 2026.";
+}
+
